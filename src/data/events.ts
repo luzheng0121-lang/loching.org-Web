74d5bd6ff8f4ffb33c0c5cf364fa756fe489bafb
luz <luz@luzdeMacBook-Pro.local>
2026-08-14 01:37:30 +0800

    feat: rebuild portfolio pages and archives

---
+++ src/data/events.ts
@@ -0,0 +1,437 @@
+export type Event = {
+  date: string;
+  name?: string | null;
+  people?: string;
+  place: string;
+  archive?: {
+    note: string;
+    images?: string[];
+  };
+};
+
+export const events: Event[] = [
+  {
+    "date": "2026.05.02–04",
+    "name": "南鹼週年慶",
+    "people":"盧政 Lu Zheng, 人形A / r_zhenssr, Aplx, Overlap Δ, Nerve, 夏杰xiajie, 呂芃, Mo&Suze陳, projeK (安若瑜 sandy), Kuthi Jin, Red, Dilian, Hohn&keyu&一榕, 小肥羊俱樂部, Puddlekillz, Blinkington City, Exfany MK II, Tsuen Wan Industrial, HeriticsAltar (Installation)" ,
+    "place": "南鹼alkaline, 廣州",
+    "archive": {
+      "note": "重建過程中",
+      "images": []
+    }
+  },
+  {
+    "date": "2026.01.18",
+    "name": null,
+
+    "place": "傀佬邪工作室, 深圳"
+  },
+  {
+    "date": "2026.01.10",
+    "name": null,
+    "people": "盧政 Lu Zheng, Anton Ponomarev, Nezok",
+    "place": "鼠窩MiceSpot, 佛山"
+  },
+  {
+    "date": "2025.12.16",
+    "name": "狂人日報 Madman pao",
+    "people": "盧政 Lu Zheng, Dasom Beak, 蕭愚 xiao",
+    "place": "扶光寰宇, 廣州"
+  },
+  {
+    "date": "2025.12.13",
+    "name": "狂人日報 Madman pao",
+    "people": "盧政 Lu Zheng, 趙子毅 zhao ziyi",
+    "place": "扶光寰宇, 廣州"
+  },
+  {
+    "date": "2025.10.10",
+    "name": "狂人日報 Madman pao",
+    "people": "盧政 Lu Zheng, 趙子毅 zhao ziyi, PGR (Paolo Gàiba Riva), 蕭愚 xiao, 嚼, yuri",
+    "place": "扶光寰宇, 廣州"
+  },
+  {
+    "date": "2025.10.09",
+    "name": null,
+    "people": "盧政 Lu Zheng, PGR (Paolo Gàiba Riva)",
+    "place": "舊天堂書店, 深圳"
+  },
+  {
+    "date": "2025.09.11",
+    "name": null,
+    "people": "盧政 Lu Zheng, 秋同 Qiutong",
+    "place": "dv, 蘇州"
+  },
+  {
+    "date": "2025.09.09",
+    "name": null,
+    "people": "盧政 Lu Zheng, phantom africa",
+    "place": "自由咖啡improv bar, 紹興"
+  },
+  {
+    "date": "2025.09.07",
+    "name": null,
+    "people": "盧政 Lu Zheng + yuri, phantom africa, SYNSO, Titus Barker",
+    "place": "肆幺幺, 紹興"
+  },
+  {
+    "date": "2025.09.06",
+    "name": "回南 fit none # 29",
+    "people": "盧政 Lu Zheng, 秋同 Qiutong, fractal, 伊帆 Yifan, 徐程 Xu Cheng, 朱楠 Zhu Nan, Titus Barker",
+    "place": "新寫作超市, 上海"
+  },
+  {
+    "date": "2025.08.30",
+    "name": "回南 fit none # 28",
+    "people": "盧政 Lu Zheng + yuri, 賣賣 MAI mai",
+    "place": "明室書房 Ming Room, 上海"
+  },
+  {
+    "date": "2025.08.30",
+    "name": "回南 fit none # 26",
+    "people": "盧政 Lu Zheng + Yuri, Titus Barker",
+    "place": "南鹼alkaline, 廣州"
+  },
+  {
+    "date": "2025.08.05",
+    "name": "你和我 You and Me Festival 3rd",
+    "people": "盧政 Lu Zheng (CN), Aaron Dilloway (US), Alice Kemp (UK), Alice Kemp + Rudolf Eb.er (UK/CH/JP), ASTROJOKE (HK), ばねとりこ Banetoriko (JP), Chaulichi (TH), Dayoon Lee (KR), 芸能佐伯組 Entertainment Saeki Group (JP), Fairy Ring (CN), FEN (JP/CN/KR/SGP), Government Alpha (JP), 肩係膊頭 Head has Shoulder (HK), 浦裕幸 Hiroyuki Ura (JP), id m theft able (US), 翰忠演奏科內利烏斯·卡迪尤《大學》, John Wilton play Cornelius Cardew \"The Great Learning\", ju sei (JP), 澀谷浩次三重奏 with Jherek Lazo Koji Shibuya Trio, LIKE WEEDS (UK/JP), 李松 Li Song (CN), 李維思 Li Weisi (CN), Lonny Hoffmann (US), Mo (HK), 大城真 Makoto Oshiro + 川口貴大 Takahiro Kawaguchi, 大友良英 Otomo Yoshihide + Sachiko M (JP), Pere Xirau (ESP), Peter Farrar (AU), P.I.G.S. (ITA/CN), rezenico (CN), Red (HK), Risaripa (JP), Rudolf Eb.er (CH/JP), サヨ族 Sayozoku (JP), Simon Whetham (UK), 夏の大△ The Great Triangle (JP), THE RITA (CA), THE RITA + 趙子毅 Zhao Ziyi (CA/CN), The Dilloways (US), 中村としまる Toshimaru Nakamura (JP), uncle123 (HK), USISI (CN), welainej (HK), 響 + 于名晶 Xiang+Yu Mingjing (CN), 幸運天使母 Lucky Angel Mother (CN), 伊帆 Yi fan (CN), 魚 Yuu (CN), yuri (RUS), 趙從 Zhao Cong (CN), 趙子毅 Zhao Ziyi (CN)",
+    "place": "榮耀東方乒乓球俱樂部小射箭館, 北京"
+  },
+  {
+    "date": "2025.07.26",
+    "name": null,
+    "people": "盧政 Lu Zheng, 林志英, 蕭愚 xiao, 林茵",
+    "place": "扶光寰宇, 廣州"
+  },
+  {
+    "date": "2025.06.14",
+    "name": null,
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, projeK (安若瑜 sandy), 姚春暘",
+    "place": "廣州光芒, 廣州"
+  },
+  {
+    "date": "2025.06.07",
+    "name": "晶體現場Jingti#12",
+    "people": "盧政 Lu Zheng, Tom Borax, 梓楊, 劉翰文, hannah, 蕭愚 xiao",
+    "place": "華僑城oct, 深圳"
+  },
+  {
+    "date": "2025.06.02",
+    "name": null,
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, projeK (安若瑜 sandy), hannah, 林茵, 蕭愚 xiao",
+    "place": "東鵬陶瓷舊工業廠區m2場, 佛山"
+  },
+  {
+    "date": "2025.05.30",
+    "name": null,
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, projeK (安若瑜 sandy), hannah, 林茵, 蕭愚 xiao",
+    "place": "蛇口k11, 深圳"
+  },
+  {
+    "date": "2025.05.24",
+    "name": null,
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, NZk",
+    "place": "鼠窩MiceSpot, 佛山"
+  },
+  {
+    "date": "2025.05.16",
+    "name": "晶體現場Jingti#11",
+    "people": "盧政 Lu Zheng, 孫一舟, projeK (安若瑜 sandy), Hannah, 咿呀嘰喳機(babbling machine), 林茵",
+    "place": "學林雅苑, 深圳"
+  },
+  {
+    "date": "2025.05.10",
+    "name": "晶體現場Jingti#10",
+    "people": "盧政 Lu Zheng, 響, hannah, 宇軒, 林茵, 咿呀嘰喳機(babbling machine), 何光旭 guang xu",
+    "place": "學林雅苑, 深圳"
+  },
+  {
+    "date": "2025.05.01",
+    "name": "崩決音樂節",
+    "people": "盧政 Lu Zheng, 久齡, ddb樂隊, 彌聲樂隊, 林志英, 張廣先, PRUSHA, 林植, 老貓與朋友, 守麥, 胡向前, Hannah, 蕭愚 xiao, 林茵, 光旭, 馬繼蘭, 陳岡",
+    "place": "崩決村, 清遠"
+  },
+  {
+    "date": "2025.04.19",
+    "name": "晶體現場Jingti#9",
+    "people": "盧政 Lu Zheng, 蕭愚 xiao, 何光旭 guang xu, 咿呀嘰喳機(babbling machine), projeK (安若瑜 sandy), hannah",
+    "place": "華僑城Oct, 深圳"
+  },
+  {
+    "date": "2025.04.12",
+    "name": "回南 fit none # 18",
+    "people": "盧政 Lu Zheng, klaus, dubdubbee)))), 買一送一 Buy One Get One",
+    "place": "舞廳, 廣州"
+  },
+  {
+    "date": "2025.03.21",
+    "name": "晶體現場Jingti#8",
+    "people": "盧政 Lu Zheng, MiMi at Capricious, VÍZ(Réka Csiszér), yuri, 陳雨桐 Yutong, 買一送一 Buy One Get One, 幸運天使母, 咿呀嘰喳機(babbling machine), 何光旭 guang xu, 趙子毅 zhao ziyi",
+    "place": "南山某地橋底, 深圳"
+  },
+  {
+    "date": "2025.03.15",
+    "name": "回南 fit none # 13",
+    "people": "盧政 Lu Zheng, C.I.A. Débutante 中情局新人組, 羅沁霖 Luo Qinlin, Celebrate Indian Breakfast 西樂布特印度暴食, 人形A",
+    "place": "portal, 廣州"
+  },
+  {
+    "date": "2025.03.15",
+    "name": null,
+    "people": "盧政 Lu Zheng, Bianco, 姚春暘, hannaH",
+    "place": "老虎上樹, 廣州"
+  },
+  {
+    "date": "2025.02.16",
+    "name": "晶體現場Jingti#7",
+    "people": "盧政 Lu Zheng, vkhnc, 何光旭 guang xu, 咿呀嘰喳機(babbling machine), 買一送一 Buy One Get One",
+    "place": "前海某地橋底, 深圳"
+  },
+  {
+    "date": "2025.01.07",
+    "name": "晶體現場Jingti#4",
+    "people": "盧政 Lu Zheng, 人形A / r_zhenssr",
+    "place": "學林雅苑, 深圳"
+  },
+  {
+    "date": "2025.01.05",
+    "name": "晶體現場Jingti#2",
+    "people": "盧政 Lu Zheng, 咿呀嘰喳機(babbling machine), 虹, 何光旭 guang xu, 買一送一 Buy One Get One, 健康, 汪政德, hannaH, 人形A",
+    "place": "南山某地橋洞, 深圳"
+  },
+  {
+    "date": "2024.12.23",
+    "name": "千禾基金會演出單元",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu",
+    "place": "怡樂社區, 廣州"
+  },
+  {
+    "date": "2024.12.21",
+    "name": "千禾基金會演出單元",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu",
+    "place": "銀林生態農場, 廣州"
+  },
+  {
+    "date": "2024.12.19",
+    "name": "千禾基金會演出單元",
+    "people": "盧政 Lu Zheng",
+    "place": "怡樂社區, 廣州"
+  },
+  {
+    "date": "2024.12.15",
+    "name": "推手",
+    "people": "盧政 Lu Zheng, Levi Liauw, 何光旭 guang xu, hannaH",
+    "place": "舊天堂書店, 深圳"
+  },
+  {
+    "date": "2024.12.15",
+    "name": "晶體現場Jingti#1",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, hannaH, projeK (安若瑜 sandy), 買一送一 Buy One Get One",
+    "place": "學林雅苑, 深圳"
+  },
+  {
+    "date": "2024.12.14",
+    "name": "千禾基金會演出單元",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu",
+    "place": "小禾的家, 怡樂社區, 廣州"
+  },
+  {
+    "date": "2024.12.07",
+    "name": "千禾基金會演出單元",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu",
+    "place": "怡樂社區, 廣州"
+  },
+  {
+    "date": "2024.12.04",
+    "name": "千禾基金會演出單元",
+    "people": "盧政 Lu Zheng, mafmadmaf",
+    "place": "紅房子, 怡樂社區, 廣州"
+  },
+  {
+    "date": "2024.10.12",
+    "name": null,
+    "people": "盧政 Lu Zheng, 買一送一 Buy One Get One, 陳浩",
+    "place": "閣樓 attic space, 廣州"
+  },
+  {
+    "date": "2024.09.21",
+    "name": null,
+    "people": "盧政 Lu Zheng, 買一送一 Buy One Get One, huhu(短跑小說)",
+    "place": "閣樓 attic space, 廣州"
+  },
+  {
+    "date": "2024.08.30",
+    "name": null,
+    "people": "盧政 Lu Zheng, 翰忠",
+    "place": "portal, 廣州"
+  },
+  {
+    "date": "2024.08.01–09",
+    "name": "你和我 You and Me Festival 2nd",
+    "people": "李劍鴻 + 盧政 Li Jianhong+Lu Zheng, Arrington De Dionyso (工作坊 workshop + 現場表演 Live), 亞馬遜先生 Amazonia, O Selvagem, 阿笛 Adi, 愛組 LOVE song, ASTROJOKE + Liz, ASTROJOKE + Red, 不演了 No Performance, 別峰 + 趙丙杲 Bie Feng + Zhao Binggao, BBBBBBB, 大鬼眾 Ghostmass, DJ Consequence, DESTROMO (工作坊 workshop + 現場表演 Live), Dope Purple from the Dark Planet feat.Tga, 丁晨晨 + 樹 Ding Chenchen + Shu, 傀佬邪 Ghost Old Evil, 光旭 HGX, KK manga, 遠藤一元 Kazumoto Endo, 空間現代 Kukangendai, 聾民池 Deaf Mire, 李亦蕾 Li Yilei + 高書藝 Gao Shuyi, 老登新爵士樂團 Old Bastard New Jazz Orchestra, 賣賣 Maimai, 梅志勇 Mei Zhiyong, 買一送一 & 果園雞 Buy One Get One Free & Orchard Chicken, Mr Marcaille, MAI MAO, 虐待護士 Torturing Nurse, Negro Leo, 二一八才！！！！NI-HAO!!!!, Praymate, 人形 A Ren Xing A (工作坊 workshop + 現場表演 Live), 水晶の舟 Suishou No Fune, 孫一舟 Sun Yizhou, SBB, stelongoria, Satanicpornocultshop, 天國注射 Tengokuchtusha, 浦裕幸 Hiroyuki Ura (工作坊 Workshop with 朱文博 Zhu Wenbo + 現場表演 Live), 雨屁 UHE, Victoria Shen a.k.a. Evicshen, 王子衡 Wang Ziheng, 午睡 Popol Wu (工作坊 workshop), Waltz time, 徐程 Xu Cheng, 鄉村即興委員會 Rural Improvisational Committee, 徐子匡窮 xzkq, 一組國際快訊 yizu international kuaixun, 意識反饋團 C.F.G (工作坊 workshop + 現場表演 Live), 姚春暘 Yao Chunyang, YAMAHAHA, 魚和喬喬 yuyu and qiao, 于劉晶 Yo Rijan, 趙子毅 Zhao Ziyi + SBB, 趙子毅 Zhao Ziyi + 遠藤一元 Kazumoto Endo",
+    "place": "榮耀東方乒乓球俱樂部小射箭館, 北京"
+  },
+  {
+    "date": "2024.07.27",
+    "name": null,
+    "people": "盧政 Lu Zheng",
+    "place": "明堂士多, 成都"
+  },
+  {
+    "date": "2024.05.08",
+    "name": null,
+    "people": "盧政 Lu Zheng, 趙子毅 zhao ziyi, 陳浩",
+    "place": "閣樓 attic space, 廣州"
+  },
+  {
+    "date": "2024.01.21",
+    "name": null,
+    "people": "舊天堂書店, 深圳",
+    "place": "（無參演名單）"
+  },
+  {
+    "date": "2024.01.20",
+    "name": null,
+    "people": "閣樓 attic space, 廣州",
+    "place": "（無參演名單）"
+  },
+  {
+    "date": "2023.11.18",
+    "name": "Pray For Sound 中國巡演",
+    "people": "碼頭組",
+    "place": "聲音共和Live House, 廣州"
+  },
+  {
+    "date": "2023.10.22",
+    "name": null,
+    "people": "碼頭組, 有話, 賽科隆",
+    "place": "聲音共和Live House, 廣州"
+  },
+  {
+    "date": "2023.09.24",
+    "name": "碼頭組專場",
+    "people": "",
+    "place": "池塘之底, 廣州"
+  },
+  {
+    "date": "2023.09.23",
+    "name": null,
+    "people": "碼頭組",
+    "place": "雷尼live house, 肇慶"
+  },
+  {
+    "date": "2023.09.08",
+    "name": "朱文博·趙從·李松 南中國巡演",
+    "people": "盧政 Lu Zheng, 秋同",
+    "place": "池塘之底, 廣州"
+  },
+  {
+    "date": "2023.09.07",
+    "name": "三個獨奏，一個三重奏",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, 顏峻",
+    "place": "舊天堂書店, 深圳"
+  },
+  {
+    "date": "2023.08.08",
+    "name": "五金微現場#18",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu",
+    "place": "朗家胡同26號, 北京"
+  },
+  {
+    "date": "2023.08.04–06",
+    "name": "你和我 You and Me Festival 1st",
+    "people": "盧政 Lu Zheng+何光旭 guang xu, 碼頭組, 日常 Waltz time, 徐子匡窮，李劍鴻 + 韋瑋，朱松傑 + 費史，AAA 即興山莊，音喜，孫一舟 + 趙子毅，般若湯(讓讓, 盧政 Lu Zheng)，朱文博 + 趙叢，顏峻 + 秋同，路新配，雞巴鯊魚，蘇維埃波普，碼頭組，河邊走，滿洲飛兒子，盧政 + 光旭，蟲齒，阿科，顏峻，盛潔，潘晨農，傀佬邪，張泰銘，宋佳蔚，趙丙杲，俊辰，翰忠，乒乓",
+    "place": "榮耀東方乒乓球俱樂部小射箭館, 北京"
+  },
+  {
+    "date": "2023.08.04",
+    "name": null,
+    "people": "碼頭組, 張泰銘, 蟲蟲",
+    "place": "ufo space, 北京"
+  },
+  {
+    "date": "2023.08.03",
+    "name": null,
+    "people": "碼頭組",
+    "place": "原料空間, 北京"
+  },
+  {
+    "date": "2023.07.14",
+    "name": "friends of hell vol.2",
+    "people": "碼頭組, Muderpsycho, 綠斑鳩, Crater",
+    "place": "池塘之底, 廣州"
+  },
+  {
+    "date": "2023.07.08",
+    "name": null,
+    "people": "盧政 Lu Zheng, 何光旭 guang xu",
+    "place": "池塘之底, 廣州"
+  },
+  {
+    "date": "2023.05.30",
+    "name": null,
+    "people": "盧政 Lu Zheng",
+    "place": "識相影棚, 廣州"
+  },
+  {
+    "date": "2023.05.21",
+    "name": null,
+    "people": "碼頭組, 廁紙樂隊",
+    "place": "ALSO LIVEHOUSE, 佛山"
+  },
+  {
+    "date": "2023.04.11",
+    "name": "噪音與玫瑰",
+    "people": "碼頭組, huhu(短跑小說), 下行通道",
+    "place": "HOU LIVE, 深圳"
+  },
+  {
+    "date": "2023.02.26",
+    "name": "客家母湯",
+    "people": "盧政 Lu Zheng, 何光旭 guang xu, 秋同",
+    "place": "Yist Club, 廣州"
+  },
+  {
+    "date": "2022.09.30",
+    "name": "friends of hell",
+    "people": "碼頭組, 榕樹河, 綠斑鳩, 惡魔十一子, Crater",
+    "place": "奧音空間, 廣州"
+  },
+  {
+    "date": "2022.09.25",
+    "name": "遷徙的日子",
+    "people": "聾民池, 短跑小說",
+    "place": "ok center music, 廣州"
+  },
+  {
+    "date": "2022.09.03",
+    "name": "廣州雜草音樂節",
+    "people": "碼頭組",
+    "place": "不大空間, 廣州"
+  },
+  {
+    "date": "2022.08.02",
+    "name": null,
+    "people": "碼頭組",
+    "place": "奧音空間, 廣州"
+  },
+  {
+    "date": "2022.07.19",
+    "name": "SXY誰想演",
+    "people": "碼頭組",
+    "place": "小洲藝術區, 廣州"
+  },
+  {
+    "date": "2022.06.25",
+    "name": "橋底意外音樂會",
+    "people": "碼頭組, 敗類老鼠, 綠斑鳩, Dictator Boy",
+    "place": "小洲藝術區, 廣州"
+  },
+  {
+    "date": "2022.05.21",
+    "name": "鬥法",
+    "people": "般若湯(讓讓, 盧政 Lu Zheng)",
+    "place": "小洲藝術區, 廣州"
+  },
+];
